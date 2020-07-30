50f425e8b85cf10649da16cf90cd03fa502a8f74
mohssineAboutaj <mohssine@mohssine-HP-Compaq-6200.(none)>
2020-07-30 16:07:50 +0100

    add mongo, add some markups

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const expEjsLayouts = require("express-ejs-layouts");
 const port = process.env.PORT || 6600;
+const { connect } = require("mongoose");
+const { mongoURI } = require("./config/keys");
+const { urlencoded } = express;
 
 // init the app
 const app = express();
 
-// Use middlewars
+// Setup Mongodb
+connect(mongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+	.then(() => {
+		console.log("Mongodb connected");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+// Setup Ejs
 app.use(expEjsLayouts);
 app.set("view engine", "ejs");
+
+// Setup bodyParser
+app.use(urlencoded({ extended: false }));
 
 // Set routes
 app.use("/", require("./routes/index"));
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,77 @@
 const route = require("express").Router();
 
+// data
+let loginFields = [
+	{
+		label: "email address",
+		id: "email-input",
+		type: "email",
+		name: "email",
+		placeholder: "Enter your email address",
+		smallText: "Help name text",
+		icon: "at",
+	},
+	{
+		label: "password",
+		id: "password-input",
+		type: "password",
+		name: "password",
+		placeholder: "******",
+		smallText: "Help password text",
+		icon: "lock",
+	},
+];
+
+let registerFields = [
+	{
+		label: "name",
+		id: "name-input",
+		type: "text",
+		name: "name",
+		placeholder: "Enter your name",
+		smallText: "Help name text",
+		icon: "user",
+	},
+	{
+		label: "email address",
+		id: "email-input",
+		type: "email",
+		name: "email",
+		placeholder: "Enter your email address",
+		smallText: "Help name text",
+		icon: "at",
+	},
+	{
+		label: "password",
+		id: "pass1-input",
+		type: "password",
+		name: "pass1",
+		placeholder: "Enter a new password",
+		smallText: "Help pass1 text",
+		icon: "lock",
+	},
+	{
+		label: "re-password",
+		id: "pass2-input",
+		type: "password",
+		name: "pass2",
+		placeholder: "Confirm your password",
+		smallText: "Help pass2 text",
+		icon: "lock",
+	},
+];
+
+// routes
 route.get("/dashboard", (req, res, next) => {
 	res.render("dashboard");
 });
 
 route.get("/login", (req, res, next) => {
-	res.render("login");
+	res.render("login", { fields: loginFields });
 });
 
 route.get("/register", (req, res, next) => {
-	res.render("register");
+	res.render("register", { fields: registerFields });
 });
 
 module.exports = route;
