c72dcfd7126308edf01e4bec9064ab18bb61d050
mohssineAboutaj <mohssine@mohssine-HP-Compaq-6200.(none)>
2020-08-02 22:47:21 +0100

    complete Registeration

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,8 @@
 const route = require("express").Router();
+const isEmpty = require("is-empty");
+const isEmail = require("is-email");
+const User = require("../module/User");
+const { genSaltSync, hashSync } = require("bcryptjs");
 
 // data
 let loginFields = [
@@ -8,7 +12,7 @@ let loginFields = [
 		type: "email",
 		name: "email",
 		placeholder: "Enter your email address",
-		smallText: "Help name text",
+		required: true,
 		icon: "at",
 	},
 	{
@@ -17,7 +21,7 @@ let loginFields = [
 		type: "password",
 		name: "password",
 		placeholder: "******",
-		smallText: "Help password text",
+		required: true,
 		icon: "lock",
 	},
 ];
@@ -29,7 +33,8 @@ let registerFields = [
 		type: "text",
 		name: "name",
 		placeholder: "Enter your name",
-		smallText: "Help name text",
+		required: true,
+		value: "",
 		icon: "user",
 	},
 	{
@@ -38,7 +43,8 @@ let registerFields = [
 		type: "email",
 		name: "email",
 		placeholder: "Enter your email address",
-		smallText: "Help name text",
+		required: true,
+		value: "",
 		icon: "at",
 	},
 	{
@@ -47,7 +53,8 @@ let registerFields = [
 		type: "password",
 		name: "pass1",
 		placeholder: "Enter a new password",
-		smallText: "Help pass1 text",
+		required: true,
+		value: "",
 		icon: "lock",
 	},
 	{
@@ -56,7 +63,8 @@ let registerFields = [
 		type: "password",
 		name: "pass2",
 		placeholder: "Confirm your password",
-		smallText: "Help pass2 text",
+		required: true,
+		value: "",
 		icon: "lock",
 	},
 ];
@@ -66,12 +74,75 @@ route.get("/dashboard", (req, res, next) => {
 	res.render("dashboard");
 });
 
+/**
+ * @name Login-Routes
+ */
 route.get("/login", (req, res, next) => {
 	res.render("login", { fields: loginFields });
 });
+route.post("/login", (req, res, next) => {
+	res.send(req.body);
+});
+// Login-Routes
+
+/**
+ * @name Register-Routes
+ */
+// a custom function to re-render register form
+function reRenderPage(res, fields, errors, name, email) {
+	fields.find((el) => el.name == "name").value = name;
+	fields.find((el) => el.name == "email").value = email;
+	res.render("register", { fields, errors });
+}
 
 route.get("/register", (req, res, next) => {
 	res.render("register", { fields: registerFields });
 });
+route.post("/register", (req, res, next) => {
+	let errors = [];
+	let { name, email, pass1, pass2 } = req.body;
+	const minNameLength = 4;
+	const minPassLength = 5;
 
+	// check & filter inputs
+	if (isEmpty(name) || (isEmpty(email) && isEmpty(pass1))) {
+		errors.push("please fill the require fields");
+	} else {
+		if (name.length < minNameLength) {
+			errors.push(`name must be greater than ${minNameLength} characters`);
+		}
+		if (!isEmail(email)) {
+			errors.push("email filed must contain a valid email");
+		}
+		if (pass1.length < minPassLength) {
+			errors.push(`password must be greater than ${minPassLength} characters`);
+		} else if (pass1 !== pass2) {
+			errors.push("password do not match");
+		}
+	}
+
+	// check & re-render the page
+	if (isEmpty(errors)) {
+		User.findOne({ email: email }).then((user) => {
+			if (user) {
+				errors.push("email already exists");
+
+				reRenderPage(res, registerFields, errors, name, email);
+			} else {
+				const salt = genSaltSync(10);
+				const password = hashSync(pass1, salt);
+
+				let newUser = new User({ name, email, password });
+				newUser.save().then((user) => {
+					res.redirect("/login");
+				});
+			}
+		});
+	} else {
+		reRenderPage(res, registerFields, errors, name, email);
+	}
+});
+// Register-Routes
+
+// export routes
 module.exports = route;
